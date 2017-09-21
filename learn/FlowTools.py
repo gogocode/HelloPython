75005ffd03eadd04cf190ab7a08828131034cad1
Tony Ho <tonyho@86shop.com.tw>
2017-09-21 15:57:37 +0800

    edit

--- learn/FlowTools.py
+++ learn/FlowTools.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env pytho
 
-import string
-
 x = int(input("Please enter an integer: "))
 
 if x < 0:
     print('Negative changed to zero')
 elif x == 0:
     print('Zero')
-elif x == 1:
-    print('Single')
-else:
-    print('More')
 
-hello test
+
+
